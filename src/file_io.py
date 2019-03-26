9e2b73a6550aaf2f29efc5f498389ab387480f87
David Freitag <freitagdavid@outlook.com>
2019-03-25 22:28:00 -0400

    file_io: Complete

--- src/file_io.py
+++ src/file_io.py
@@ -10,9 +10,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('foo.txt') as f:
+    print(f.read())
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacus.\n"
+
+with open('bar.txt', 'w+') as f:
+    for i in range(0, 3):
+        f.write(text)
+
+with open('bar.txt') as f:
+    print(f.read())
